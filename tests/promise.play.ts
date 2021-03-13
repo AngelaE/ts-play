98c213cbcf5be3575bc27a6f7e9c8cdca06213ad
Angela <angela.koepp@gmail.com>
2021-03-14 12:45:56 +1300

    camelCase method name

--- tests/promise.play.ts
+++ tests/promise.play.ts
@@ -1,9 +1,9 @@
 // This sample file demonstrates some simple patterns on how to use promises.
 // These are not real tests!
-// Reason for this format is that we had problems with lost promises in 
+// Reason for this format is that we had problems with lost promises in
 // after hooks and the tests are an easy way to run lots of different scenarios.
 
-async function SlowCall(name: string, timeoutMs: number): Promise<string> {
+async function slowCall(name: string, timeoutMs: number): Promise<string> {
   console.log(`Level ${name} started, timeout ${timeoutMs}`);
   await new Promise((resolve) => setTimeout(resolve, timeoutMs));
   console.log(`Level ${name} finished`);
@@ -13,8 +13,8 @@ async function SlowCall(name: string, timeoutMs: number): Promise<string> {
 describe('Samples of waiting for promises - no real tests', () => {
   it('Wait for two promises after each other', async () => {
     console.log('test entry');
-    console.log(await SlowCall('1', 100));
-    console.log(await SlowCall('2', 50));
+    console.log(await slowCall('1', 100));
+    console.log(await slowCall('2', 50));
     console.log('test entry');
   });
 
@@ -25,7 +25,7 @@ describe('Samples of waiting for promises - no real tests', () => {
       { name: '2 - Array.foreach', ms: 150 },
     ];
     callsToMake.forEach(async (call) => {
-      console.log('Result: ', await SlowCall(call.name, call.ms));
+      console.log('Result: ', await slowCall(call.name, call.ms));
     });
     console.log('test exit');
   });
@@ -37,7 +37,7 @@ describe('Samples of waiting for promises - no real tests', () => {
       { name: '2', ms: 50 },
     ];
     for (const call of callsToMake) {
-      console.log('Result: ', await SlowCall(call.name, call.ms));
+      console.log('Result: ', await slowCall(call.name, call.ms));
     }
     console.log('test exit');
   });
@@ -49,7 +49,7 @@ describe('Samples of waiting for promises - no real tests', () => {
       { name: '2', ms: 50 },
     ];
 
-    const allPromises = callsToMake.map((call) => SlowCall(call.name, call.ms));
+    const allPromises = callsToMake.map((call) => slowCall(call.name, call.ms));
     const results = await Promise.all(allPromises);
     console.log('All promises are resolved');
     results.forEach((result) => console.log('Result: ', result));
@@ -59,7 +59,7 @@ describe('Samples of waiting for promises - no real tests', () => {
   it('Wait for promises running parallel V2', async () => {
     console.log('test entry');
 
-    const allPromises = [SlowCall('1', 100), SlowCall('2', 50)];
+    const allPromises = [slowCall('1', 100), slowCall('2', 50)];
     const results = await Promise.all(allPromises);
     console.log('All promises are resolved');
     results.forEach((result) => console.log('Result: ', result));
@@ -69,7 +69,7 @@ describe('Samples of waiting for promises - no real tests', () => {
   it('Wait for first promise', async () => {
     console.log('test entry');
 
-    const allPromises = [SlowCall('1', 100), SlowCall('2', 50)];
+    const allPromises = [slowCall('1', 100), slowCall('2', 50)];
     const result = await Promise.race(allPromises);
     console.log('First promise is resolved');
     console.log('Result: ', result);
